21ceeaeef2c801ea2674a97fab2e85763469b985
Awes0meM4n <lonchezas@hotmail.com>
2020-03-12 14:46:16 +0100

    Commit inicial

---
+++ src/main/java/es/lanyu/datosdeportivosapi/DatosdeportivosapiApplication.java
@@ -0,0 +1,13 @@
+package es.lanyu.datosdeportivosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatosdeportivosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatosdeportivosapiApplication.class, args);
+	}
+
+}
