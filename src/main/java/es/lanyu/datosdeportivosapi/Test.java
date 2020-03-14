dabe14d66152d3c55cfa1f5a59820b9df582f102
Awes0meM4n <lonchezas@hotmail.com>
2020-03-14 12:17:26 +0100

    Usando @Value en constructor

--- src/main/java/es/lanyu/datosdeportivosapi/Test.java
+++ src/main/java/es/lanyu/datosdeportivosapi/Test.java
@@ -5,12 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component("anotacion")
 public class Test extends es.lanyu.Test{
-
+	
 	@Value("${datosdeportivos.testString}")
 	String stringPropiedad;
 	
-    public Test() {
-        testString = stringPropiedad;// No se tiene acceso en el constructor
+//    public Test() {
+//        testString = stringPropiedad;// No se tiene acceso en el constructor
+//    }
+    
+    public Test(@Value("${valor.parametro}")// Para usarlo en constructor tiene que ser por parametro
+    			String stringPropiedad) { // Usa el constructor sin parametro si tambien existe
+        System.out.println(stringPropiedad);
     }
     
     public void init() {
