bad376f93dfc0fef588e8dc20235f72fe5a69f4b
Awes0meM4n <lonchezas@hotmail.com>
2020-03-14 12:55:22 +0100

    Uso varios ficheros properties con @PropertySource

--- src/main/java/es/lanyu/datosdeportivosapi/DatosdeportivosapiApplication.java
+++ src/main/java/es/lanyu/datosdeportivosapi/DatosdeportivosapiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 import es.lanyu.Test;
 import es.lanyu.config.JavaConfig;
@@ -12,6 +13,7 @@ import es.lanyu.config.JavaConfig;
 @SpringBootApplication
 @ImportResource({"config.xml"})
 @Import({JavaConfig.class})
+@PropertySource({"application2.properties"})
 public class DatosdeportivosapiApplication {
 
 	public static void main(String[] args) {
