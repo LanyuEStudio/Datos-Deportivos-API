81b34742e576980f3f4cb3bf2e86edaefc221ea7
Awes0meM4n <lonchezas@hotmail.com>
2020-03-17 22:02:34 +0100

    Marcando constructor a usar

--- src/main/java/es/lanyu/datosdeportivosapi/TestAutowired.java
+++ src/main/java/es/lanyu/datosdeportivosapi/TestAutowired.java
@@ -2,6 +2,7 @@ package es.lanyu.datosdeportivosapi;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import es.lanyu.Test;
@@ -12,7 +13,10 @@ public class TestAutowired extends Test {
 	private final Logger log = LoggerFactory.getLogger(TestAutowired.class);
 
 	Test testInyectado;
+
+	public TestAutowired() {}
 	
+	@Autowired
 	public TestAutowired(es.lanyu.datosdeportivosapi.Test testPorConstructor) {
 		testInyectado = testPorConstructor;
 	}
