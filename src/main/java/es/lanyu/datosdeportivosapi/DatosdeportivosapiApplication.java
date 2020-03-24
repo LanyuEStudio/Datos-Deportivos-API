201685dc71a9c707d1990693aba3e89ced09adf5
Awes0meM4n <lonchezas@hotmail.com>
2020-03-24 14:09:43 +0100

    Solución práctica hasta ORM básico

--- src/main/java/es/lanyu/datosdeportivosapi/DatosdeportivosapiApplication.java
+++ src/main/java/es/lanyu/datosdeportivosapi/DatosdeportivosapiApplication.java
@@ -1,5 +1,8 @@
 package es.lanyu.datosdeportivosapi;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -11,6 +14,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
+import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.lanyu.participante.Participante;
+import es.lanyu.participante.repositorios.ParticipanteDAO;
 import es.lanyu.usuarios.repositorios.Usuario;
 import es.lanyu.usuarios.repositorios.UsuarioDAO;
 
@@ -26,12 +36,43 @@ public class DatosdeportivosapiApplication {
 		ConfigurableApplicationContext context =
 				SpringApplication.run(DatosdeportivosapiApplication.class, args);
 		
-		UsuarioDAO usuarioDAO = context.getBean(UsuarioDAO.class);
-		usuarioDAO.save(generaUsuario());
-		List<Usuario> usuarios = usuarioDAO.findByCorreoContaining("5");
-		usuarios.stream().map(Usuario::toString).forEach(log::info);
+//		UsuarioDAO usuarioDAO = context.getBean(UsuarioDAO.class);
+//		usuarioDAO.save(generaUsuario());
+//		List<Usuario> usuarios = usuarioDAO.findByCorreoContaining("6");
+//		usuarios.stream().map(Usuario::toString).forEach(log::info);
+		
+		ObjectMapper mapper = context.getBean(ObjectMapper.class);
+		mapper.addMixIn(Participante.class, MixInParticipantes.class);
+		ParticipanteDAO participanteDAO = context.getBean(ParticipanteDAO.class);
+		cargarParticipanteDesdeArchivo("src/main/resources/participantes.json", mapper, participanteDAO);
+		List<Participante> participantes = participanteDAO.findByNombreContaining("Real");
+		participantes.stream().map(Participante::toString).forEach(log::trace);
 		
 		context.close();
+	}
+	
+	@JsonIgnoreProperties(value = { "hashCode" })
+	abstract class MixInParticipantes {
+		@JsonProperty("id")
+		abstract String getIdentificador();
+	}
+	
+	static void cargarParticipanteDesdeArchivo(String ruta, ObjectMapper mapper, ParticipanteDAO participanteDAO) {
+		String linea = null;
+		mapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		try (BufferedReader buffer = new BufferedReader(
+				new InputStreamReader(new FileInputStream(ruta), "UTF-8"))) {
+			Participante participante;
+			while((linea = buffer.readLine()) != null) {
+				if (linea.startsWith("{") && linea.endsWith("}")) {
+					participante = mapper.readValue(linea, Participante.class);
+					participanteDAO.save(participante);
+//					log.trace("Cargado {}", participante);
+				}
+			}
+		} catch (Exception e) {
+			log.error("Error leyendo: {}", linea);
+		}
 	}
 	
 	static Usuario generaUsuario() {
--- src/main/java/es/lanyu/participante/repositorios/ParticipanteDAO.java
+++ src/main/java/es/lanyu/participante/repositorios/ParticipanteDAO.java
@@ -1,5 +1,7 @@
 package es.lanyu.participante.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import es.lanyu.participante.Participante;
 @Repository
 public interface ParticipanteDAO extends JpaRepository<Participante, String> {
 
+	List<Participante> findByNombreContaining(String txt);
 }
