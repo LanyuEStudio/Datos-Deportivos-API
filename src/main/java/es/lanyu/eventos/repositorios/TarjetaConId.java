4ce88421c65186181c6a7505d34acf881bd130fb
Awes0meM4n <lonchezas@hotmail.com>
2020-03-31 20:39:28 +0200

    Implemento SINGLE_TABLE por XML (Paso 2)

--- src/main/java/es/lanyu/eventos/repositorios/TarjetaConId.java
+++ src/main/java/es/lanyu/eventos/repositorios/TarjetaConId.java
@@ -3,6 +3,7 @@ package es.lanyu.eventos.repositorios;
 import es.lanyu.comun.suceso.Tarjeta;
 import es.lanyu.comun.suceso.TarjetaImpl.TipoTarjeta;
 
+// Mapeo ORM en resources/jpa/SucesoConId.xml
 public class TarjetaConId extends SucesoConId implements Tarjeta {
 	
 	private TipoTarjeta tipoTarjeta;
