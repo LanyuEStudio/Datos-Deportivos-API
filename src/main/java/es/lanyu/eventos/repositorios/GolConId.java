fc59fc76d58b5a8465fd3f058394983771ddb0ac
Awes0meM4n <lonchezas@hotmail.com>
2020-03-31 20:40:12 +0200

    ORM de GolConId por anotaciones (Paso 3)

--- src/main/java/es/lanyu/eventos/repositorios/GolConId.java
+++ src/main/java/es/lanyu/eventos/repositorios/GolConId.java
@@ -1,8 +1,16 @@
 package es.lanyu.eventos.repositorios;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
 import es.lanyu.comun.suceso.Gol;
 import es.lanyu.participante.Participante;
 
+@Entity
+@Access(value=AccessType.FIELD)
+@DiscriminatorValue("G")
 public class GolConId extends SucesoConId implements Gol {
 	
 	@Override
