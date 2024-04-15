dabbd01674f042c8f22726f394916ea07be0c0be
Awes0meM4n <lonchezas@hotmail.com>
2024-04-15 12:18:59 +0200

    Mejoras en ConfiguracionRest
    * Cambiada la forma de crear los endpoints "search"
    * Reemplazado `setAllowedOrigins` por `setAllowedOriginPatterns` en CorsFilter
    * Añadido aceptar el header `Authorization`

--- src/main/java/es/lanyu/eventos/rest/ConfiguracionRest.java
+++ src/main/java/es/lanyu/eventos/rest/ConfiguracionRest.java
@@ -19,6 +19,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.RepositorySearchesResource;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.cors.CorsConfiguration;
@@ -60,21 +61,23 @@ public class ConfiguracionRest {
             @Override
             public RepositorySearchesResource process(RepositorySearchesResource searchResource) {
                 if (controllersRegistrados.containsKey(searchResource.getDomainType())) {
-                    Method[] metodos = controllersRegistrados.get(searchResource.getDomainType()).getDeclaredMethods();
+                    Class<?> controller = controllersRegistrados.get(searchResource.getDomainType());
+                    Method[] metodos = controller.getDeclaredMethods();
+                    URI uriController = linkTo(controller).toUri();
+                    String controllerPath = config.getBasePath() + uriController.getPath();
                     for (Method m : metodos) {
-                        if (!m.isAnnotationPresent(ResponseBody.class)) {
+                        if (!m.isAnnotationPresent(ResponseBody.class) || !m.isAnnotationPresent(GetMapping.class)) {
                             continue;
                         }
                         try {
-                            URI uri = linkTo(m).toUri();
-                            String path = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(),
-                                    config.getBasePath() + uri.getPath(), null, null).toString();
-                            path = URLDecoder.decode(path, "UTF-8");
+                            String pathMetodo = String.join("", m.getAnnotation(GetMapping.class).value());
+                            String pathRecurso = new URI(uriController.getScheme(), uriController.getUserInfo(), uriController.getHost(),
+                                                         uriController.getPort(), controllerPath + pathMetodo, null, null).toString();
                             String requestParams = Stream.of(m.getParameters())
-                                    .filter(p -> p.isAnnotationPresent(RequestParam.class))
-                                    .map(Parameter::getName)
-                                    .collect(Collectors.joining(","));
-                            searchResource.add(Link.of(path + "{?" + requestParams + "}", m.getName()));
+                                                         .filter(p -> p.isAnnotationPresent(RequestParam.class))
+                                                         .map(Parameter::getName)
+                                                         .collect(Collectors.joining(","));
+                            searchResource.add(Link.of(URLDecoder.decode(pathRecurso, "UTF-8") + "{?" + requestParams + "}", m.getName()));
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -100,8 +103,8 @@ public class ConfiguracionRest {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
 
